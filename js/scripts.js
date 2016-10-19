7c010b8f8b5107ab25266da1d02fda8503718126
Chris Wingo and Lina Shadrach <lina@>
2016-10-19 16:51:57 -0700

    make sure all the functions are working correctly. Output still needs styling.

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,28 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var firstNameInput = $("input#firstName").val();
-    var lastNameInput = $("input#lastName").val();
+    var lastNameInput = $("#lastName").val();
     var bornInput = $("input#born").val();
     var favoriteColorInput = $("input#favoriteColor").val();
-    var timeOfDayInput = $("input#timeOfDay").val();
+    var timeOfDayInput = $("#timeOfDay").val();
     var candidateInput = $("input:radio[name=candidate]:checked").val();
 
     $(".firstName").text(firstNameInput);
     $(".lastName").text(lastNameInput);
     $(".born").text(bornInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".timeOfDay").text(timeOfDayInput);
+
+  $(".blankSurvey").toggle();
+  $(".results").toggle();
+  $("body").css('background-color', favoriteColorInput);
+  if (candidateInput === "clinton"){
+    $("#clinton").toggle();
+  };
+  if (candidateInput === "trump"){
+    $("#trump").toggle();
+  };
+  if (candidateInput === "other"){
+    $("#other").toggle();
+  };
   });
-
-if (candidateInput === "clinton"){
-
-};
 });
